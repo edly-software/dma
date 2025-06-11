4fe7487955c7fa27626c81e846e6180f7b0f29f7
livghit <liviu.g01@gmail.com>
2025-06-11 20:41:08 +0200

    changed from bun to npm

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -232,11 +232,11 @@ export const MobileNavToggle = ({
 export const NavbarLogo = () => {
   return (
     <a
-      href="/"
+      href="/dma"
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
     >
       <img
-        src="/logo.jpeg"
+        src="/dma/logo.jpeg"
         alt="logo"
         width={30}
         height={30}
